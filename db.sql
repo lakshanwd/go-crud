2942d2567d4e6338eafa4a5f29e9027e10f7cb7d
Supun Dissanayake <supunlakshandissanayake@gmail.com>
2018-01-28 01:45:19 +0530

    changed database strucure

--- db.sql
+++ db.sql
@@ -5,15 +5,15 @@ CREATE DATABASE library;
 USE library;
 
 CREATE TABLE `tbl_book` (
-  `book_id` int(11) NOT NULL,
-  `book_name` varchar(45) NOT NULL,
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(45) NOT NULL UNIQUE,
   `book_author` varchar(45) NOT NULL,
   PRIMARY KEY (`book_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tbl_student` (
   `student_id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_name` varchar(45) NOT NULL,
+  `student_name` varchar(45) NOT NULL UNIQUE,
   `student_age` int(11) NOT NULL,
   `street_address` varchar(45) DEFAULT NULL,
   `street_address_2` varchar(45) DEFAULT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `tbl_student` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tbl_student_book` (
-  `student_book_id` int(11) NOT NULL,
+  `student_book_id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   KEY `fk_student` (`student_id`),
